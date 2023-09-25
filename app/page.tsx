725d7c1072a2ab01b87339e00c12fd115f75c07e
MELLAHKAIS <mellahkais@MELLAHKAISs-MacBook-Pro.local>
2023-09-25 15:00:43 +0100

    finsihed events

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,40 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import styles from "./page.module.css";
+
+import Whyus from "./component/home/whyus";
+import Counter from "./component/home/counter";
+import Menu from "./component/home/menu";
+import Testimonial from "./component/home/testimonial";
+import Chef from "./component/home/chef";
+import Booking from "./component/home/booking";
+import Gallery from "./component/home/gallery";
+import Header from "./component/home/header";
+import Hero from "./component/home/hero";
+import ContactUs from "./component/home/contactUs";
+import Footer from "./component/home/footer";
+import Event from "./component/home/event";
+import About from "./component/home/about";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <Header />
+      <Hero />
+      <main id="main" className={styles.main}>
+        <About/>
+        <Whyus />
+        <Counter />
+        <Menu />
+        <Testimonial />
+        <Event />
+        <Chef />
+        <Booking />
+        <Gallery />
+        <ContactUs />
+      </main>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <Footer />
+    </>
+  );
 }
